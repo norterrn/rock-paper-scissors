949de44fac4d81c4a360123900af9e21ed1b7483
norterrn <andrea.pasquariello2002@gmail.com>
2022-08-23 23:36:18 +0200

    Added the rest of js code, games restarts after clicking play again

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ let rockEl = document.getElementById("rock-el");
 let paperEl = document.getElementById("paper-el");
 let scissorsEl = document.getElementById("scissors-el");
 let resultEl = document.getElementById("result-el");
-let playerScoreEl = document.getElementById("playerscore-el")
-let computerScoreEl = document.getElementById("computerscore-el")
-let resultPlayer = 0
-let resultComputer = 0
+let playerScoreEl = document.getElementById("playerscore-el");
+let computerScoreEl = document.getElementById("computerscore-el");
+let resultPlayer = 0;
+let resultComputer = 0;
+let playAgainButton = document.getElementById("rematch")
+
+playAgainButton.toggleAttribute("hidden");
+
 
 // Get computer random choice
 function getComputerChoice() {
@@ -16,56 +20,70 @@ function getComputerChoice() {
 }
 
 
-function main() {
+function playRound(playerSelection) {
+  let computerPick = getComputerChoice();
+  console.log(`${playerSelection} vs ${computerPick}`);
 
-  rockEl.addEventListener("click", function () {
-    let computerPick = getComputerChoice()
-    console.log("rock vs " + computerPick)
-    if (computerPick === "paper"){
-        resultEl.textContent = "Result: You lose. Paper beats rock"
-        resultComputer += 1
-        computerScoreEl.textContent ="Computer's score: " + resultComputer
-    } else if (computerPick === "scissors"){
-        resultEl.textContent = "Result: You win. Rock beats scissors";
-        resultPlayer += 1
-        playerScoreEl.textContent = "Player's score: " + resultPlayer;
-    } else {
-      resultEl.textContent = "Result: Tie."
-    }
-  });
-
-  paperEl.addEventListener("click", function () {
-    let computerPick = getComputerChoice();
-    console.log("paper vs " + computerPick);
-    if (computerPick === "scissors") {
-      resultEl.textContent = "Result: You lose. Scissors beat paper";
-      resultComputer += 1;
-      computerScoreEl.textContent = "Computer's score: " + resultComputer;
-    } else if (computerPick === "rock") {
-      resultEl.textContent = "Result: You win. Paper beats rock";
+  // tu zrob ify, sprawdz czym jest playerSelection i computerPick, wyswietl wynik i dodaj pkt
+  if (playerSelection === "scissors" && computerPick === "paper"){
+      resultEl.textContent = "Result: Scissors beat paper"
+      resultPlayer += 1
+      playerScoreEl.textContent = "Player's score: "+ resultPlayer
+  } else if (playerSelection === "paper" && computerPick === "rock"){
+      resultEl.textContent = "Result: Paper beats rock"
       resultPlayer += 1;
-      playerScoreEl.textContent = "Player's score: " + resultPlayer;
-    } else {
-      resultEl.textContent = "Result: Tie.";
-    }
-  });
-
-  scissorsEl.addEventListener("click", function () {
-    let computerPick = getComputerChoice();
-    console.log("scissors vs " + computerPick);
-    if (computerPick === "rock") {
-      resultEl.textContent = "Result: You lose. Rock beats scissors";
-      resultComputer += 1;
-      computerScoreEl.textContent = "Computer's score: " + resultComputer;
-    } else if (computerPick === "paper") {
-      resultEl.textContent = "Result: You win. Scissors beat paper";
+      playerScoreEl.textContent = "Player's score: " + resultPlayer
+  } else if (playerSelection === "rock" && computerPick === "scissors"){
+      resultEl.textContent = "Result: Rock beats scissors"
       resultPlayer += 1;
-      playerScoreEl.textContent = "Player's score: " + resultPlayer;
-    } else {
-      resultEl.textContent = "Result: Tie.";
-    }
-  });
+      playerScoreEl.textContent = "Player's score: " + resultPlayer
+  } else if (playerSelection === computerPick){
+      resultEl.textContent = "Result: Tie"
+  } else {
+    resultComputer +=1
+    computerScoreEl.textContent ="Computer's score: " + resultComputer
+    resultEl.textContent = "Lost to a bot lmao"
+  }
+  }
+
+function checkPoints() {
+
+  if (resultPlayer === 5) {
+    resultEl.textContent = "You won!! The score is " + resultPlayer + " to " + resultComputer;
+    playAgainButton.toggleAttribute("hidden");
+    rockEl.toggleAttribute("hidden");
+    paperEl.toggleAttribute("hidden");
+    scissorsEl.toggleAttribute("hidden");
+    
+
+  } else if(resultComputer === 5){
+    resultEl.textContent = "You lost... The score is " + resultPlayer + " to " + resultComputer;
+    playAgainButton.toggleAttribute("hidden");
+  }
+  
 }
 
-main()
+rockEl.addEventListener("click", function () {
+  playRound("rock");
+  checkPoints();
+});
+paperEl.addEventListener("click", function () {
+  playRound("paper");
+  checkPoints();
+});
+scissorsEl.addEventListener("click", function () {
+  
+});
+
+playAgainButton.addEventListener("click", function () {
+  // wyzeruj pkt, pokaz przyciski i schowaj przycisk zagraj ponownie
+  resultComputer = 0
+  resultPlayer = 0
+  playerScoreEl.textContent = "Player's score: ";
+  computerScoreEl.textContent = "Computer's score: ";
+  resultEl.textContent = "Result:"
+  rockEl.toggleAttribute("hidden");
+  paperEl.toggleAttribute("hidden");
+  scissorsEl.toggleAttribute("hidden");
+});
 
